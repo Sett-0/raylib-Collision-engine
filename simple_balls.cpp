0f88f65f99a1653ee99e77f85e4d27f56348d84c
Sett-0 <ampilov2000@mail.ru>
2026-06-07 07:37:05 +0300

    Add iterations to collision resolution.

--- simple_balls.cpp
+++ simple_balls.cpp
@@ -1,5 +1,6 @@
 #include <ctime>
 #include <vector>
+
 #include "raylib.h"
 #include "raymath.h"
 
@@ -9,6 +10,7 @@
 #define HEIGHT 800
 #define GRIDSIZE_X 4
 #define GRIDSIZE_Y 4
+#define ITERATIONS 4
 
 struct BallInitData {
 	float R;
@@ -52,6 +54,10 @@ private:
 	
 };
 
+struct CollisionGrid {
+	std::vector<Ball*> grid[GRIDSIZE_X][GRIDSIZE_Y];
+};
+
 Ball makeRandomBall() {
 	float R  = GetRandomValue(10, 100);
 	float x  = GetRandomValue(R, WIDTH - R);
@@ -75,39 +81,39 @@ std::vector<Ball> makeRandomBalls(const int N) {
 	return balls;
 }
 
-void clearGrid(std::vector<Ball*> (&grid)[GRIDSIZE_X][GRIDSIZE_Y]) {
+void clearGrid(CollisionGrid &collisionGrid) {
 	for (size_t x = 0; x < GRIDSIZE_X; x++)
 		for (size_t y = 0; y < GRIDSIZE_Y; y++)
-			grid[x][y].clear();
+			collisionGrid.grid[x][y].clear();
 }
 
-void populateGrid(std::vector<Ball*> (&grid)[GRIDSIZE_X][GRIDSIZE_Y], std::vector<Ball> &balls) {
+void populateGrid(CollisionGrid &collisionGrid, std::vector<Ball> &balls) {
 	for (Ball &ball : balls) {
 		int cellX = ball.coord.x / (WIDTH  / GRIDSIZE_X);
 		int cellY = ball.coord.y / (HEIGHT / GRIDSIZE_Y);
-		grid[cellX][cellY].push_back(&ball);
+		collisionGrid.grid[cellX][cellY].push_back(&ball);
 		
 		// Checking if the ball touches neighboring cells
-		int lCellX = (ball.coord.x - ball.R) / (WIDTH  / GRIDSIZE_X);
-		int rCellX = (ball.coord.x + ball.R) / (WIDTH  / GRIDSIZE_X);
-		int tCellY = (ball.coord.y - ball.R) / (HEIGHT / GRIDSIZE_Y);
-		int bCellY = (ball.coord.y + ball.R) / (HEIGHT / GRIDSIZE_Y);
+		int lCell = (ball.coord.x - ball.R) / (WIDTH  / GRIDSIZE_X);
+		int rCell = (ball.coord.x + ball.R) / (WIDTH  / GRIDSIZE_X);
+		int tCell = (ball.coord.y - ball.R) / (HEIGHT / GRIDSIZE_Y);
+		int bCell = (ball.coord.y + ball.R) / (HEIGHT / GRIDSIZE_Y);
 		// Top-left
-		if (lCellX > 0 && tCellY > 0 && lCellX < cellX && tCellY < cellY) { grid[lCellX][tCellY].push_back(&ball); }
+		if (lCell > 0 && tCell > 0 && lCell < cellX && tCell < cellY) { collisionGrid.grid[lCell][tCell].push_back(&ball); }
 		// Top
-		if (tCellY > 0 && tCellY < cellY) { grid[cellX][tCellY].push_back(&ball); }
+		if (tCell > 0 && tCell < cellY) { collisionGrid.grid[cellX][tCell].push_back(&ball); }
 		// Top-right
-		if (rCellX < GRIDSIZE_X && tCellY > 0 && rCellX > cellX && tCellY < cellY) { grid[rCellX][tCellY].push_back(&ball); }
+		if (rCell < GRIDSIZE_X && tCell > 0 && rCell > cellX && tCell < cellY) { collisionGrid.grid[rCell][tCell].push_back(&ball); }
 		// Right
-		if (rCellX < GRIDSIZE_X && rCellX > cellX) { grid[rCellX][cellY].push_back(&ball); }
+		if (rCell < GRIDSIZE_X && rCell > cellX) { collisionGrid.grid[rCell][cellY].push_back(&ball); }
 		// Bottom-right
-		if (rCellX < GRIDSIZE_X && bCellY < GRIDSIZE_Y && rCellX > cellX && bCellY > cellY) { grid[rCellX][bCellY].push_back(&ball); }
+		if (rCell < GRIDSIZE_X && bCell < GRIDSIZE_Y && rCell > cellX && bCell > cellY) { collisionGrid.grid[rCell][bCell].push_back(&ball); }
 		// Bottom
-		if (rCellX < GRIDSIZE_X && bCellY > cellY) { grid[cellX][bCellY].push_back(&ball); }
+		if (bCell < GRIDSIZE_Y && bCell > cellY) { collisionGrid.grid[cellX][bCell].push_back(&ball); }
 		// Bottom-left
-		if (lCellX > 0 && bCellY < GRIDSIZE_Y && lCellX < cellX && bCellY > cellY) { grid[lCellX][bCellY].push_back(&ball); }
+		if (lCell > 0 && bCell < GRIDSIZE_Y && lCell < cellX && bCell > cellY) { collisionGrid.grid[lCell][bCell].push_back(&ball); }
 		// Left
-		if (lCellX > 0 && lCellX < cellX) { grid[lCellX][cellY].push_back(&ball); }
+		if (lCell > 0 && lCell < cellX) { collisionGrid.grid[lCell][cellY].push_back(&ball); }
 	}
 }
 
@@ -123,52 +129,53 @@ void onCollision(Vector2 &coord, Vector2 &speed) {
 	speed = Vector2Scale(speed, DRAG);
 }
 
-void resolveCollisions(std::vector<Ball*> (&grid)[GRIDSIZE_X][GRIDSIZE_Y]) {
+void resolveCollisions(CollisionGrid &collisionGrid) {
 	// Window bounds
 	Vector2 lBound[] = { { 0,     0      }, { 0,     HEIGHT } };
 	Vector2 rBound[] = { { WIDTH, 0      }, { WIDTH, HEIGHT } };
 	Vector2 tBound[] = { { 0,     0      }, { WIDTH, 0      } };
 	Vector2 bBound[] = { { 0,     HEIGHT }, { WIDTH, HEIGHT } };			
-	
-	for (size_t x = 0; x < GRIDSIZE_X; x++) {
-		for (size_t y = 0; y < GRIDSIZE_Y; y++) {
-			auto &bucket = grid[x][y];
-			for (size_t i = 0; i < bucket.size(); i++) {
-				// Collision type: Ball : lBound
-				if (x == 0) {
-					if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, lBound[0], lBound[1])) {
-						onCollision(bucket[i]->coord.x, bucket[i]->speed.x);
+	for (size_t i = 0; i < ITERATIONS; i++) {
+		for (size_t x = 0; x < GRIDSIZE_X; x++) {
+			for (size_t y = 0; y < GRIDSIZE_Y; y++) {
+				auto &bucket = collisionGrid.grid[x][y];
+				for (size_t i = 0; i < bucket.size(); i++) {
+					// Collision type: Ball : lBound
+					if (x == 0) {
+						if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, lBound[0], lBound[1])) {
+							onCollision(bucket[i]->coord.x, bucket[i]->speed.x);
+						}
 					}
-				}
-				
-				// Collision type: Ball : rBound
-				if (x == GRIDSIZE_X-1) {
-					if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, rBound[0], rBound[1])) {
-						onCollision(bucket[i]->coord.x, bucket[i]->speed.x);
+					
+					// Collision type: Ball : rBound
+					if (x == GRIDSIZE_X-1) {
+						if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, rBound[0], rBound[1])) {
+							onCollision(bucket[i]->coord.x, bucket[i]->speed.x);
+						}
 					}
-				}
-				
-				// Collision type: Ball : tBound
-				if (y == 0) {
-					if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, tBound[0], tBound[1])) {
-						onCollision(bucket[i]->coord.y, bucket[i]->speed.y);
+					
+					// Collision type: Ball : tBound
+					if (y == 0) {
+						if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, tBound[0], tBound[1])) {
+							onCollision(bucket[i]->coord.y, bucket[i]->speed.y);
+						}
 					}
-				}
-				
-				// Collision type: Ball : bBound
-				if (y == GRIDSIZE_Y-1) {
-					if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, bBound[0], bBound[1])) {
-						onCollision(bucket[i]->coord.y, bucket[i]->speed.y);
+					
+					// Collision type: Ball : bBound
+					if (y == GRIDSIZE_Y-1) {
+						if (CheckCollisionCircleLine(bucket[i]->coord, bucket[i]->R, bBound[0], bBound[1])) {
+							onCollision(bucket[i]->coord.y, bucket[i]->speed.y);
+						}
 					}
-				}
-				
-				if (bucket.size() > 1) {
-					for (size_t j = i + 1; j < bucket.size(); j++) {
+					
+					if (bucket.size() > 1) {
 						// Collision type: Ball : Ball
-						if (bucket[i] == bucket[j]) continue;
-						if (CheckCollisionCircles(bucket[i]->coord, bucket[i]->R, bucket[j]->coord, bucket[j]->R)) {
-							onCollision(bucket[i]->coord, bucket[i]->speed);
-							onCollision(bucket[j]->coord, bucket[j]->speed);
+						for (size_t j = i + 1; j < bucket.size(); j++) {
+							if (bucket[i] == bucket[j]) continue;
+							if (CheckCollisionCircles(bucket[i]->coord, bucket[i]->R, bucket[j]->coord, bucket[j]->R)) {
+								onCollision(bucket[i]->coord, bucket[i]->speed);
+								onCollision(bucket[j]->coord, bucket[j]->speed);
+							}
 						}
 					}
 				}
@@ -181,11 +188,11 @@ int main() {
 	InitWindow(WIDTH, HEIGHT, "raylib Balling");
 	SetTargetFPS(60);
 	SetRandomSeed(std::time(0));
-	std::vector<Ball*> grid[GRIDSIZE_X][GRIDSIZE_Y];
+	CollisionGrid grid{};
 	
 	//auto balls = makeRandomBalls(10);
-	Ball ball1 = Ball(100, { WIDTH/2 - 300, HEIGHT/2 }, { 0, 0 }, RED);
-	Ball ball2 = Ball(100, { WIDTH/2 + 300, 0        }, { 0, 0 }, BLUE);
+	Ball ball1 = Ball(100, { WIDTH/2, HEIGHT/2 }, { 0, 0 }, RED);
+	Ball ball2 = Ball(100, { WIDTH/2, HEIGHT-101 }, { 0, 0 }, BLUE);
 	std::vector<Ball> balls = { ball1, ball2 };
 	
 	while (!WindowShouldClose()) {
@@ -224,13 +231,28 @@ int main() {
 			
 			// Drawing ball
 			DrawCircleV(ball.coord, ball.R, ball.color);
+			
+			// Extra logic 
+			// if (ball.coord.y + ball.R >= HEIGHT && FloatEquals(ball.speed.y, 0))
+			// {
+				// ball.speed.y = 0;
+				// ball.coord.y = HEIGHT - ball.R;
+			// }
+			// if (HEIGHT - ball.coord.y < ball.R) {
+				// ball.speed.x *= DRAG;
+			// }
 		}
 		
-		DrawText(TextFormat("x:  %f", balls[0].coord.x), 20, 20,  30, BLACK);
-		DrawText(TextFormat("y:  %f", balls[0].coord.y), 20, 50,  30, BLACK);
-		DrawText(TextFormat("dx: %f", balls[0].speed.x), 20, 80,  30, BLACK);
-		DrawText(TextFormat("dy: %f", balls[0].speed.y), 20, 110, 30, BLACK);
+		DrawText(TextFormat(" x: %s%f", balls[0].coord.x >= 0 ? " " : "", balls[0].coord.x), 20, 20,  30, BLACK);
+		DrawText(TextFormat(" y: %s%f", balls[0].coord.y >= 0 ? " " : "", balls[0].coord.y), 20, 50,  30, BLACK);
+		DrawText(TextFormat("dx: %s%f", balls[0].speed.x >= 0 ? " " : "", balls[0].speed.x), 20, 80,  30, BLACK);
+		DrawText(TextFormat("dy: %s%f", balls[0].speed.y >= 0 ? " " : "", balls[0].speed.y), 20, 110, 30, BLACK);
 		DrawText(TextFormat("Color: %d %d %d", balls[0].color.r, balls[0].color.g, balls[0].color.b), 20, 140, 30, BLACK);
+		DrawText(TextFormat(" x: %s%f", balls[1].coord.x >= 0 ? " " : "", balls[1].coord.x), WIDTH-220-20, 20,  30, BLACK);
+		DrawText(TextFormat(" y: %s%f", balls[1].coord.y >= 0 ? " " : "", balls[1].coord.y), WIDTH-220-20, 50,  30, BLACK);
+		DrawText(TextFormat("dx: %s%f", balls[1].speed.x >= 0 ? " " : "", balls[1].speed.x), WIDTH-220-20, 80,  30, BLACK);
+		DrawText(TextFormat("dy: %s%f", balls[1].speed.y >= 0 ? " " : "", balls[1].speed.y), WIDTH-220-20, 110, 30, BLACK);
+		DrawText(TextFormat("Color: %d %d %d", balls[1].color.r, balls[1].color.g, balls[1].color.b), WIDTH-220-20, 140, 30, BLACK);
 		EndDrawing();
 	}
 	CloseWindow();
